7be310d30efebccd21146b733b248ee1e65d550d
nglsena0722c <nglsena0722@com2us.com>
2023-08-28 15:56:02 +0900

    Fix : Fix Contract Address

--- src/components/contractAddress.ts
+++ src/components/contractAddress.ts
@@ -1,5 +1,5 @@
- const contractAddress = "xpla15g7usr6h3htmnrhege3s2z6sg4d8k066pvp05lp24qy3w5p2svmqtra0vq"; // dot : 5 
+//  const contractAddress = "xpla15g7usr6h3htmnrhege3s2z6sg4d8k066pvp05lp24qy3w5p2svmqtra0vq"; // dot : 5 
 // const contractAddress = "xpla1yszm7sykd8wcd7wfe9srtujdmxq2t99xkwss9qzxp5c8p4yyqr4qtqn4ew"  // dot : 10
-// const contractAddress = "xpla137jkvee9nvc0ye6m43r336c28cvjl8z0cjtghg5vcj3w0svp4xwqxgkjnj"  // dot : 10
+const contractAddress = "xpla137jkvee9nvc0ye6m43r336c28cvjl8z0cjtghg5vcj3w0svp4xwqxgkjnj"  // dot : 10
 
 export default contractAddress;
