714cecd7d7f6b0459a63920103b75f3d1586a1c2
nglsena0722c <nglsena0722@com2us.com>
2023-08-28 12:42:46 +0900

    Feat : Design MyDot

--- src/components/MyDot/index.tsx
+++ src/components/MyDot/index.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { CircularProgress } from "@mui/material";
-import {
-  ConnectedWallet,
-  useConnectedWallet,
-} from "@xpla/wallet-provider";
+import { ConnectedWallet, useConnectedWallet } from "@xpla/wallet-provider";
 import useUserDots from "../useQuery/useUserDots";
 import useContractConfig, { Config } from "../useQuery/useContractConfig";
 import useLatestBlock from "../useQuery/useLatestBlock";
+import Card from "../Card";
+import clsx from "clsx";
 
 export interface Dot {
   X: number;
@@ -17,7 +16,7 @@ export interface Dot {
   painted_block?: number;
 }
 
-const Claim = () => {
+const MyDot = () => {
   const contractAddress =
     "xpla15g7usr6h3htmnrhege3s2z6sg4d8k066pvp05lp24qy3w5p2svmqtra0vq";
   const connectedWallet = useConnectedWallet();
@@ -29,9 +28,18 @@ const Claim = () => {
     <CircularProgress />
   ) : (
     <>
+      <Card
+        title="Block Info"
+        children={
+          <>
+            <div>Latest Block : {latestBlock}</div>
+            {!loadingContractConfig && (
+              <div>Lock Block Height : {contractConfig.lock_block_height}</div>
+            )}
+          </>
+        }
+      />
       <br />
-      <div>Latest Block : {latestBlock}</div>
-      <div></div>
       <UserDotsData
         contractConfig={contractConfig}
         contractAddress={contractAddress}
@@ -61,22 +69,31 @@ const UserDotsData = ({
   return isLoading ? (
     <CircularProgress />
   ) : (
-    <>
-      {userDotsData.map((datum) => (
-        <SingleDotData
-          key={datum.x + datum.y }
-          contractConfig={contractConfig}
-          contractAddress={contractAddress}
-          connectedWallet={connectedWallet}
-          latestBlock={latestBlock}
-          datum={datum}
-        />
-      ))}
-    </>
+    <div
+      className={clsx(
+        "mt-8 px-8 flex flex-row items-center max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      )}
+    >
+      <div className="p-5 w-full">
+        <h5 className="w-full flex justify-between items-center  mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <span>Your Lock Info :</span>
+        </h5>
+        {userDotsData.map((datum) => (
+          <SingleDotData
+            key={datum.x + datum.y}
+            contractConfig={contractConfig}
+            contractAddress={contractAddress}
+            connectedWallet={connectedWallet}
+            latestBlock={latestBlock}
+            datum={datum}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default Claim;
+export default MyDot;
 
 const SingleDotData = ({
   contractAddress,
@@ -90,29 +107,35 @@ const SingleDotData = ({
   contractConfig: Config;
   latestBlock: string;
   datum: {
-      x: number;
-      y: number;
-      lock: {
-        denom: string;
-        amount: string;
-      };
-      dot_owner: string;
-      color: string;
-      unlocked : boolean;
-    }
-  ;
+    x: number;
+    y: number;
+    lock: {
+      denom: string;
+      amount: string;
+    };
+    dot_owner: string;
+    color: string;
+    unlocked: boolean;
+  };
 }) => {
   return (
     <>
       <div className="flex w-full justify-between">
-        <div>
-          ------- <br />
+        <div className="w-full">
+        <div className="w-full my-4" style={{ border: "1px solid gray" }} />
           dot - X : {datum.x} <br />
           dot - Y : {datum.y} <br />
           color : {datum.color} <br />
-          {datum.lock ? <>Lock Amount : {datum.lock?.amount} <br /></> : <>No Lock<br/></> }
-          
-          ------- <br />
+          {datum.lock ? (
+            <>
+              Lock Amount : {datum.lock?.amount} <br />
+            </>
+          ) : (
+            <>
+              No Lock
+              <br />
+            </>
+          )}
         </div>
       </div>
     </>
